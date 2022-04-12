244fbda3242d62237eb07ca158117feb8d3e647c
AimakhanSaken <99489326+AimakhanSaken@users.noreply.github.com>
2022-04-12 19:47:52 +0600

    RealEst

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+   
+        
+
+});
